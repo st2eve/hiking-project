75a1b94bfe209c9fbc8d254f9c3b561122b1ea95
st2eve <houbart.steeve@gmail.com>
2022-07-27 12:05:44 +0200

    suite register

--- src/view/includes/header.php
+++ src/view/includes/header.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link type="text/css" href="../../css/main.css" rel="stylesheet"> -->
+    <link type="text/css" href="../../css/register.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Becode - Boiler plate MVC</title>
 </head>
 <body>
     <header>
-        <h1>Welcome to my site</h1>
+        <h1>Welcome to One More Hike</h1>
     </header>
--- src/view/user/register.php
+++ src/view/user/register.php
@@ -1,4 +1,28 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-echo '<h1>Congratulations ! tu es sur la page inscription</h1>';
+require '../view/includes/header.php';
+?>
+<body>
+    <div class="main-block">
+      <h1>Registration</h1>
+      <form action="/">
+        <hr>
+        <input type="text" name="name" class="name" placeholder="Email" required/>
+        <input type="text" name="name" class="name" placeholder="Firstname" required/>
+        <input type="text" name="name" class="name" placeholder="Lastname" required/>
+        <input type="text" name="name" class="name" placeholder="Username" required/>
+        <input type="password" name="name" class="name" placeholder="Password" required/>
+        <hr>
+        <div class="btn-block">
+          <button type="submit" href="/">Submit</button>
+        </div>
+      </form>
+    </div>
+  </body>
+
+  <?php
+  require '../view/includes/footer.php';
+  ?>
