567dcf262f30a66fdc9883cf6025952c7bbc8002
MrOlivierdA <olivier.dardenne.pro@gmail.com>
2022-07-29 15:26:54 +0200

    up

--- src/view/includes/footer.php
+++ src/view/includes/footer.php
@@ -2,13 +2,14 @@
 <footer>
     <section class="down">
         <div class="signature">
+        <img src="IMG/signatures.png" alt="signature" height="75px">
 
         </div>
 
 &copy; One more Hike: BeCode <?php echo date('Y')?>
 
-        <div class="medias">
-            <ul>
+        <div>
+            <ul class="medias">
                 <li>instagram</li>
                 <li>facebook</li>
                 <li>twitter</li>
