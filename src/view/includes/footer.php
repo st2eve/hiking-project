98594a7348cc85df106ac7b4415ee9e8e54aa730
MrOlivierdA <olivier.dardenne.pro@gmail.com>
2022-07-29 14:54:10 +0200

    update test

--- src/view/includes/footer.php
+++ src/view/includes/footer.php
@@ -1,5 +1,20 @@
+
 <footer>
-        &copy; BeCode <?php echo date('Y')?>
+    <section class="down">
+        <div class="signature">
+
+        </div>
+
+&copy; One more Hike: BeCode <?php echo date('Y')?>
+
+        <div class="medias">
+            <ul>
+                <li>instagram</li>
+                <li>facebook</li>
+                <li>twitter</li>
+            </ul>
+        </div>
+    </section>
     </footer>
 </body>
 </html>
